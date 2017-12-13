70a1d8da82bbdd522e13c8503acd294420f23659
Eduardo Gomes <eduardo.gomes@concrete.com.br>
2017-12-13 12:51:19 -0200

    Alteração na porta

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ http.createServer((req, res) => {
     for (let i = 1e7; i > 0; i--) {}
     console.log(`Handling request from ${pid}`);
     res.end(`Hello from ${pid}\n`);
-}).listen(3000, () => {
+}).listen(9999, () => {
     setTimeout(() => {
         throw new Error('Hum.. sujou!..');    
     }, Math.ceil(Math.random() * 10) * 1000);
